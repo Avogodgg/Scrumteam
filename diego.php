ff864d0509027f88793bf8e6f961cbaa470d364d
Tomas Gonzalez Alvarado <tomas.gonzalez@smartin.tecnm.mx>
2026-03-10 17:23:00 -0600

    mas cambios en diego.php

--- diego.php
+++ diego.php
@@ -3,6 +3,7 @@
 echo "Diego Alexis Rivas Muñoz";
 echo "Holaaaaa"
 =======
-echo "Diega Rivera Moños";
+for ($i=1; $i<200;$i++
+      echo "Diega Rivera Moños";
 >>>>>>> e8d229a8db069c73874d0cd699afebfecbbedbc4
 ?>
